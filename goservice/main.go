fb01980f693305065a0de4fa68184d36a204fe47
Bruce Horn <bruce@datawire.io>
2020-05-18 13:52:25 -0700

    checkpoint, calls between go/python services

--- goservice/main.go
+++ goservice/main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -93,14 +94,42 @@ func helloHandler2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "/hello => Hello Go World! v3")
 }
 
+func forwardHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("/fwd => about to forward to python service")
+	res, err := http.Get("http://pyservice/hello")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("     => returned from http.Get")
+	data, _ := ioutil.ReadAll(res.Body)
+	log.Printf("     => returned from ioutil.ReadAll()")
+
+	defer res.Body.Close()
+	log.Printf("     => returned from res.Body.Close()")
+
+	// Write to response
+	fmt.Fprintf(w, "pyservice/hello returned %d", res.StatusCode)
+	fmt.Fprintf(w, "\n  => %v", string(data))
+
+	log.Printf("     => finished writing to ResponseWriter")
+}
+
 
 func main() {
+	// For the demo, we can disable security checks.  Not normally recommended!
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Wire up the paths to their respective handlers
 	http.HandleFunc("/", helloHandler1)
+	http.HandleFunc("/fwd", forwardHandler)
 	http.HandleFunc("/hello", helloHandler2)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
+	// Start listening
 	fmt.Println("listening at localhost:8080")
 	fmt.Println("Try http://localhost:8080/hello")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- pyservice/src/service.py
+++ pyservice/src/service.py
@@ -13,7 +13,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     logging.info("handled /")
-    return 'Hello Python World!'
+    return '/ handled: Hello Python World!'
+
+
+@app.route('/hello')
+def hello_world_2():
+    logging.info("handled /hello")
+    return '/hello handled: Hello Python World!'
+
+
+@app.route('/fwd')
+def forward_to_go_service():
+    logging.info("handled /fwd")
+
+    # Now make a request of the go service and return its value.
+    r = requests.get("http://goservice/hello", verify=False)
+    return f"/goservice/hello returned {r.status_code} => {r.text}"
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
